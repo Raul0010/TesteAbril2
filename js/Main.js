3b62da276be4db834823c5e1778859ea37c4c2e4
Raul Vaz <raul.vaz@uol.com.br>
2018-02-02 12:00:27 -0200

    Terminado versao mobile

--- js/Main.js
+++ js/Main.js
@@ -3,9 +3,30 @@ $(document).ready(function () {
 
     $(".Mobile").hide();
     $(".Desktop").show();
+    $(".Close").hide();
+    $(".InputPesquisa").hide();
 
     if (isMobile.matches) {
         $(".Mobile").show();
         $(".Desktop").hide();
     }
+
+    $(".Open").click(function(){
+        $(".dropdown-content").show();
+        $(".Open").hide();
+        $(".Close").show();
+        $(".MudaCor").toggleClass("Vermelho");
+    });
+    
+    $(".Close").click(function(){
+        $(".dropdown-content").hide();
+        $(".Close").hide();
+        $(".Open").show();
+        $(".MudaCor").toggleClass("Vermelho");
+    });
+    
+    $(".Pesquisa").click(function(){
+        $(".ImgPesquisa").toggle();
+        $(".InputPesquisa").toggle();
+    });
 });
